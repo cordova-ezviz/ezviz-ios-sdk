e09726481d4c2666fb6908cad03c7743cdc47283
MrMessy77 <313691730@qq.com>
2017-10-22 20:25:04 +0800

    init sdk

---
+++ EZDemoSDK/EZDemoSDK.h
@@ -0,0 +1,24 @@
+//
+//  EZDemoSDK.h
+//  EZDemoSDK
+//
+//  Created by MrMessy on 2017/6/16.
+//  Copyright © 2017年 MrMessy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EZDemoSDK.
+FOUNDATION_EXPORT double EZDemoSDKVersionNumber;
+
+//! Project version string for EZDemoSDK.
+FOUNDATION_EXPORT const unsigned char EZDemoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EZDemoSDK/PublicHeader.h>
+#import <EZDemoSDK/EZOpenSDK.h>
+#import <EZDemoSDK/EZDeviceInfo.h>
+#import <EZDemoSDK/EZCameraInfo.h>
+#import <EZDemoSDK/EZLivePlayViewController.h>
+#import <EZDemoSDK/EZCameraTableViewController.h>
+#import <EZDemoSDK/EZAddByQRCodeViewController.h>
+#import <EZDemoSDK/EZWifiConfigViewController.h>
